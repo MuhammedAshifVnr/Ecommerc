2b873adb57025207a1216639ef18fa16eba44f0e
ashif <ashifpvnr@gmail.com>
2024-02-28 23:04:50 +0530

    cart duplicate fount

--- routers/users/cart.go
+++ routers/users/cart.go
@@ -11,8 +11,13 @@ import (
 
 func AddCart(c *gin.Context) {
 	id, _ := strconv.Atoi(c.Param("ID"))
-
-	cart := database.Cart{
+	var cart database.Cart
+	helper.DB.Where("product_id=? AND user_id=?", id, Find.ID).First(&cart)
+	if cart.ID != 0 {
+		c.JSON(400, "Item alredy in cart.")
+		return
+	}
+	cart = database.Cart{
 		UserID:    Find.ID,
 		ProductID: uint(id),
 		Quantity:  1,
@@ -28,6 +33,13 @@ func Cart(c *gin.Context) {
 
 		c.JSON(200, v)
 	}
+	var total float64
+	for _, v := range cart {
+		total += float64(v.Product.ProductPrize) * float64(v.Quantity)
+	}
+	c.JSON(200, gin.H{
+		"Total Amount": total,
+	})
 }
 
 func CartDelete(c *gin.Context) {
@@ -47,11 +59,11 @@ func CartQuantity(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, "Product is out of stock")
 		return
 	}
-	if quantity>10{
+	if quantity > 10 {
 		c.JSON(http.StatusBadRequest, "Only 10 unit's allowed each order")
 		return
 	}
 
-	helper.DB.Model(&database.Cart{}).Where("id=?", id).Update("Quantity",quantity)
-	c.JSON(200,"Quantity Updated.")
+	helper.DB.Model(&database.Cart{}).Where("id=?", id).Update("Quantity", quantity)
+	c.JSON(200, "Quantity Updated.")
 }
