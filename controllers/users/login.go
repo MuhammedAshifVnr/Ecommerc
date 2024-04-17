f53a4886d1d3b2a2b4c551eb6fac49c0ce70e4f2
ashif <ashifpvnr@gmail.com>
2024-04-17 12:04:15 +0530

    jwt seting

--- controllers/users/login.go
+++ controllers/users/login.go
@@ -48,7 +48,7 @@ func Login(c *gin.Context) {
 	if err != nil {
 		fmt.Println("TOken cant generate.")
 	}
-	c.SetCookie("user", token, int((time.Hour * 24).Seconds()), "/", "localhost", false, true)
+	c.SetCookie("user", token, int((time.Hour * 24).Seconds()), "/", "ashif.online", false, false)
 	fmt.Println(token)
 	c.JSON(200, gin.H{
 		"code":    200,
